3ec821846b025369b8db00aa5019488901a30698
herik-silva <herik.silva.019@gmail.com>
2023-01-04 10:33:59 -0300

    iniciado implementação da classe de erro

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from analisador_lexico import AnalisadorLexico
+# from analisador_lexico import AnalisadorLexico
+from erro import Erro
 
-scanner = AnalisadorLexico("file.txt")
+# scanner = AnalisadorLexico("file.txt")
 
-#print(len(scanner._conteudo))
-scanner.token()
+# #print(len(scanner._conteudo))
+# scanner.token()
 
 
+erro = Erro(10, 10)
+erro.exibirErro()
