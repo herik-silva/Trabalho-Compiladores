6110ddfbe90400bb42c612f1d083aa9ecc703678
Lucas Batista <lucas.bt46@gmail.com>
2023-01-14 18:42:25 -0300

    Implementação análise sintático

--- analisador_sintatico.py
+++ analisador_sintatico.py
@@ -8,21 +8,66 @@ class AnalisadorSintatico:
         self.lexico = lexico
         self.token = None
 
+
     def inicio(self):
         self.token = self.lexico.proximoToken()
         if(self.token.text == 'PRINCIPAL'):
-            print(self.token)
+            print(self.token, 'OK')
             self.token = self.lexico.proximoToken()
             if(self.token.text == '#'):
-                print(self.token)
+                print(self.token, 'ok')
                 self.listaComandos()
 
     def listaComandos(self):
-        self.comando()
+        self.token = self.lexico.proximoToken()
+        if(self.token.TK_STR[self.token.type] == 'PALAVRA RESERVADA'
+            or self.token.TK_STR[self.token.type] == 'IDENTIFICADOR'
+            or self.token.TK_STR[self.token.type] == 'NUMERO'
+            or self.token.TK_STR[self.token.type] == 'PONTUACAO'):
+                self.comando()
+                self.listaComandosl()
+
+    def listaComandosl(self):
+        self.listaComandos()
 
     def comando(self):
-        self.calMatematico()
+        if(self.token.TK_STR[self.token.type] == 'PALAVRA RESERVADA'):
+            if(self.token.text == 'INTEIRO' or self.token.text == 'TEXTO'
+                or self.token.text == 'CONSTANTE'):
+                self.declaracao()
+        if(self.token.TK_STR[self.token.type] == 'IDENTIFICADOR'):
+            self.atribuicao()
     
+    def declaracao(self):
+        self.tipo()
+        self.variavel()
+        self.declaracaol()
+
+    def declaracaol(self):
+        self.token = self.lexico.proximoToken()
+        if(self.token.TK_STR[self.token.type] == 'ATRIBUICAO'):
+            print(self.token)
+            self.conteudo()
+
+    def conteudo(self):
+        self.token = self.lexico.proximoToken()
+        if(self.token.TK_STR[self.token.type] == 'LITERAL'):
+            print(self.token)
+
+    def atribuicao(self):
+        if(self.token.TK_STR[self.token.type] == 'PALAVRA RESERVADA'):
+            if(self.token.text == 'INTEIRO' or self.token.text == 'TEXTO'
+                or self.token.text == 'CONSTANTE'):
+                self.declaracao()
+
+    def tipo(self):
+        print(self.token)
+    
+    def variavel(self):
+        self.token = self.lexico.proximoToken()
+        if(self.token.TK_STR[self.token.type] == 'IDENTIFICADOR'):
+            print(print(self.token))
+
     def calMatematico(self):
         self.E()
 
